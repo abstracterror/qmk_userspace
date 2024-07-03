acbbaa9df40b1a1a37731840e4f98171433f1f36
abstracterror <drew@abstracterror.com>
2024-07-03 21:45:00 +0100

    Rearrange home row mods for littol

--- keyboards/obabo/littol/keymaps/abstracterror/keymap.c
+++ keyboards/obabo/littol/keymaps/abstracterror/keymap.c
@@ -25,13 +25,13 @@ enum tap_dance_names {
 #define LT_TILD TD(TD_LT_TILD)
 
 #define MT_A    MT(MOD_LCTL, KC_A)
-#define MT_R    MT(MOD_LSFT, KC_R)
-#define MT_S    MT(MOD_LGUI, KC_S)
-#define MT_T    MT(MOD_LALT, KC_T)
+#define MT_R    MT(MOD_LGUI, KC_R)
+#define MT_S    MT(MOD_LALT, KC_S)
+#define MT_T    MT(MOD_LSFT, KC_T)
 
-#define MT_N    MT(MOD_LALT, KC_N)
-#define MT_E    MT(MOD_LGUI, KC_E)
-#define MT_I    MT(MOD_LSFT, KC_I)
+#define MT_N    MT(MOD_LSFT, KC_N)
+#define MT_E    MT(MOD_LALT, KC_E)
+#define MT_I    MT(MOD_LGUI, KC_I)
 #define MT_O    MT(MOD_LCTL, KC_O)
 
 #define MT_CAPS MT(MOD_LCTL, KC_CAPS)
@@ -50,19 +50,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_FUN] = LAYOUT(
         XXXXXXX, XXXXXXX, XXXXXXX, UK_TOGG, QK_BOOT,    XXXXXXX, KC_F4,   KC_F5,   KC_F6,   XXXXXXX,
-        KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, XXXXXXX,    XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F12,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT, XXXXXXX,    XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F12,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,    LT_F10,  KC_F7,   KC_F8,   KC_F9,   KC_F11
     ),
 
     [_NUM] = LAYOUT(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, KC_4,    KC_5,    KC_6,    _______,
-        KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, XXXXXXX,    XXXXXXX, KC_1,    KC_2,    KC_3,    KC_BSLS,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT, XXXXXXX,    XXXXXXX, KC_1,    KC_2,    KC_3,    KC_BSLS,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,    LT_0,    KC_7,    KC_8,    KC_9,    KC_PIPE
     ),
 
     [_RSYM] = LAYOUT(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, KC_DLR,  KC_PERC, KC_CIRC, _______,
-        KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, XXXXXXX,    XXXXXXX, KC_EXLM, KC_AT,   KC_HASH, XXXXXXX,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT, XXXXXXX,    XXXXXXX, KC_EXLM, KC_AT,   KC_HASH, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    LT_TILD, KC_AMPR, KC_ASTR, KC_GRV,  XXXXXXX
     ),
 
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_NAV] = LAYOUT(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_HOME, XXXXXXX, XXXXXXX, KC_BSPC, KC_DEL,
-        MT_CAPS, KC_LSFT, KC_LGUI, KC_LALT, KC_ESC,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT,
+        MT_CAPS, KC_LGUI, KC_LALT, KC_LSFT, KC_ESC,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,    _______, KC_PGDN, KC_PGUP, KC_END,  KC_INS
     ),
 };
