944278a5a7acbec1f9424988f3ab8adb017396d6
abstracterror <drew@abstracterror.com>
2024-07-28 23:25:20 +0100

    Make Cocot36+'s function layer consistent
    Makes the top row of the function layer in my Cocot36+ keymap consistent
    with those in my keymaps for other boards.

--- keyboards/aki27/cocot36plus_v2/keymaps/abstracterror/keymap.c
+++ keyboards/aki27/cocot36plus_v2/keymaps/abstracterror/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_FUN] = LAYOUT(
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, KC_F4,   KC_F5,   KC_F6,   XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, UK_TOGG, QK_BOOT,          XXXXXXX, KC_F4,   KC_F5,   KC_F6,   XXXXXXX,
         KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, XXXXXXX,          XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F12,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, KC_F7,   KC_F8,   KC_F9,   KC_F11,
                           XXXXXXX, XXXXXXX, _______,          LT_F10,  XXXXXXX, XXXXXXX
