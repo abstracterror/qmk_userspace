f4360da4470351c4c66fd654f91ec9c97a7b7d08
abstracterror <drew@abstracterror.com>
2024-11-30 19:40:47 +0000

    Update calvin_mcd/travaulta keymap
    * Changes bindings of the outer keys on the bottom row.
    * Removes RGB keys inherited from my Vault 35 keymap (Travaulta has no
      LEDs).
    * Adds bindings for adjusting the actuation point and clearing the EC
      settings.

--- keyboards/calvin_mcd/travaulta/keymaps/abstracterror/keymap.c
+++ keyboards/calvin_mcd/travaulta/keymaps/abstracterror/keymap.c
@@ -23,41 +23,41 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,    KC_W,    LT_E,    LT_R,    KC_T,    KC_Y,    LT_U,    KC_I,    KC_O,    KC_P,    KC_F12,
         MT_A,    MT_S,    MT_D,    MT_F,    KC_G,    KC_H,    MT_J,    MT_K,    MT_L,    MT_SCLN,
         MT_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  MT_SLSH,
-                          KC_MEH,  LT_TAB,  MT_TAB,  LT_SPC,  MO_LSYM, KC_ENT
+                          CW_TOGG, AE_MEH,  MT_TAB,  LT_SPC,  MO_LSYM, QK_REP
     ),
 
     [_FUN] = LAYOUT(
-        XXXXXXX, XXXXXXX, XXXXXXX, UK_TOGG, QK_BOOT, XXXXXXX, KC_F4,   KC_F5,   KC_F6,   XXXXXXX, _______,
+        XXXXXXX, XXXXXXX, _______, UK_TOGG, QK_BOOT, XXXXXXX, KC_F4,   KC_F5,   KC_F6,   XXXXXXX, _______,
         KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, XXXXXXX, XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F12,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F7,   KC_F8,   KC_F9,   XXXXXXX,
-                          XXXXXXX, XXXXXXX, _______, LT_F10,  KC_F11,  XXXXXXX
+                          XXXXXXX, _______, _______, LT_F10,  KC_F11,  _______
     ),
 
     [_NUM] = LAYOUT(
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_4,    KC_5,    KC_6,    _______, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, KC_4,    KC_5,    KC_6,    _______, XXXXXXX,
         KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, XXXXXXX, UM_GBP,  KC_1,    KC_2,    KC_3,    KC_BSLS,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, UM_EURO, KC_7,    KC_8,    KC_9,    KC_PIPE,
-                          XXXXXXX, XXXXXXX, _______, LT_0,    MO_TSYM, XXXXXXX
+                          XXXXXXX, _______, _______, LT_0,    MO_TSYM, _______
     ),
 
     [_LSYM] = LAYOUT(
-        XXXXXXX, KC_PLUS, KC_UNDS, KC_DQUO, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, KC_PLUS, KC_UNDS, KC_DQUO, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         KC_COLN, KC_EQL,  KC_MINS, KC_QUOT, XXXXXXX, XXXXXXX, KC_LALT, KC_LGUI, KC_LSFT, KC_LCTL,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                          XXXXXXX, MO_TSYM, _______, _______, XXXXXXX, XXXXXXX
+                          XXXXXXX, MO_TSYM, _______, _______, XXXXXXX, _______
     ),
 
     [_TSYM] = LAYOUT(
         KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_GRV,  KC_TILD, XXXXXXX,
-        KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, XXXXXXX, RGB_MOD, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_RMD, XXXXXXX, RGB_HUD, RGB_SAD, RGB_VAD,
-                          XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX
+        KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, XXXXXXX, XXXXXXX, EC_AP_I, EC_CLR,  XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, EC_AP_D, XXXXXXX, XXXXXXX, XXXXXXX,
+                          XXXXXXX, _______, _______, _______, _______, XXXXXXX
     ),
 
     [_NAV] = LAYOUT(
         XXXXXXX, PREVTAB, NEXTTAB, REDO,    CTL_BRK, XXXXXXX, XXXXXXX, KC_DEL,  KC_BSPC, XXXXXXX, XXXXXXX,
         KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, KC_ESC,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT,
         UNDO,    CUT,     COPY,    PASTE,   XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,
-                          XXXXXXX, XXXXXXX, MT_CWT,  _______, XXXXXXX, XXXXXXX
+                          XXXXXXX, XXXXXXX, MT_CWT,  _______, XXXXXXX, _______
     ),
 };
