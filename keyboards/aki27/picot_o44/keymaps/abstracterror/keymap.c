ebf724f9163dba5974626c7a29ac1600081152d9
abstracterror <drew@abstracterror.com>
2025-04-13 23:55:21 +0100

    Add keymap for aki27/picot_o44

---
+++ keyboards/aki27/picot_o44/keymaps/abstracterror/keymap.c
@@ -0,0 +1,75 @@
+/*
+Copyright 2024 @abstracterror
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include QMK_KEYBOARD_H
+#include "abstracterror.h"
+#include "ae_combos.c"
+#include "ae_keyoverrides.c"
+
+#define MO_MOUS MO(_MOUSE)
+
+const char PROGMEM chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] = LAYOUT(
+    'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R',
+    'L', 'L', 'L', 'L', 'L',           'R', 'R', 'R', 'R', 'R',
+    'L', 'L', 'L', 'L', 'L',           'R', 'R', 'R', 'R', 'R',
+    '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*'
+);
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    [_BASE] = LAYOUT(
+        KC_Q,    KC_W,    LT_F,    LT_P,    KC_B,    MS_BTN1, MS_BTN2, KC_J,    LT_L,    KC_U,    KC_Y,    KC_SCLN,
+        MT_A,    MT_R,    MT_S,    MT_T,    KC_G,                      KC_M,    MT_N,    MT_E,    MT_I,    MT_O,
+        KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                      KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,
+        XXXXXXX, XXXXXXX, XXXXXXX, AE_MEH,  MT_TAB,  XXXXXXX, LT_SPC,  XXXXXXX, QK_REP,  XXXXXXX, XXXXXXX, MO_MOUS
+    ),
+
+    [_FUN] = LAYOUT(
+        XXXXXXX, XXXXXXX, _______, UK_TOGG, QK_BOOT, _______, _______, XXXXXXX, KC_F4,   KC_F5,   KC_F6,   _______,
+        KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, XXXXXXX,                   KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F12,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   _______, XXXXXXX, KC_F7,   KC_F8,   KC_F9,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, LT_F10,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+    ),
+
+    [_NUM] = LAYOUT(
+        XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, _______, _______, XXXXXXX, KC_4,    KC_5,    KC_6,    _______,
+        KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, XXXXXXX,                   UM_GBP,  KC_1,    KC_2,    KC_3,    KC_BSLS,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   _______, UM_EURO, KC_7,    KC_8,    KC_9,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, LT_0,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+    ),
+
+    [_LSYM] = LAYOUT(
+        XXXXXXX, KC_PLUS, KC_UNDS, KC_DQUO, XXXXXXX, _______, _______, XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_COLN, KC_EQL,  KC_MINS, KC_QUOT, XXXXXXX,                   XXXXXXX, KC_LALT, KC_LGUI, KC_LSFT, KC_LCTL,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+    ),
+
+    [_NAV] = LAYOUT(
+        XXXXXXX, PREVTAB, NEXTTAB, REDO,    CTL_BRK, _______, _______, XXXXXXX, XXXXXXX, KC_DEL,  KC_BSPC, KC_INS,
+        KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, KC_ESC,                    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT,
+        UNDO,    CUT,     COPY,    PASTE,   XXXXXXX,                   _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MT_CWT,  XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+    ),
+
+    [_MOUSE] = LAYOUT(
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, SCRL_IN, XXXXXXX, XXXXXXX,                   XXXXXXX, CPI_SW,  SCRL_SW, ROT_L15, ROT_R15,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______
+    ),
+};
