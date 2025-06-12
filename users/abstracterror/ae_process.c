e59b87a36cacbef9fb6c59d9156256fb7c9bfb19
abstracterror <drew@abstracterror.com>
2025-06-12 20:01:05 +0100

    Disable flow tap for layer tap keys

--- users/abstracterror/ae_process.c
+++ users/abstracterror/ae_process.c
@@ -212,3 +212,17 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
     }
     return false;
 }
+
+uint16_t get_flow_tap_term(
+    uint16_t keycode,
+    keyrecord_t* record,
+    uint16_t prev_keycode
+) {
+    if (is_flow_tap_key(keycode) && is_flow_tap_key(prev_keycode)) {
+        if (IS_QK_LAYER_TAP(keycode)) {
+            return 0;
+        }
+        return FLOW_TAP_TERM;
+    }
+    return 0;
+}
