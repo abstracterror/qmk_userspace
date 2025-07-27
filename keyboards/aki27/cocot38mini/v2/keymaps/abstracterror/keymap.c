e1122a200b88d21f2225f6ac16140c19cf93638b
abstracterror <drew@abstracterror.com>
2025-07-27 20:55:25 +0100

    Add keymap for aki27/cocot38mini/v2

---
+++ keyboards/aki27/cocot38mini/v2/keymaps/abstracterror/keymap.c
@@ -0,0 +1,75 @@
+/*
+Copyright 2024 @abstracterror
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include QMK_KEYBOARD_H
+#include "abstracterror.h"
+#include "ae_combos.c"
+#include "ae_keyoverrides.c"
+
+#define TG_MOUS TG(_MOUSE)
+
+const char PROGMEM chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] = LAYOUT(
+    'L', 'L', 'L', 'L', 'L',      'R', 'R', 'R', 'R', 'R',
+    'L', 'L', 'L', 'L', 'L', '*', 'R', 'R', 'R', 'R', 'R',
+    'L', 'L', 'L', 'L', 'L',      'R', 'R', 'R', 'R', 'R',
+              '*', '*', '*', '*', '*', '*', '*'
+);
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    [_BASE] = LAYOUT(
+        KC_Q,    KC_W,    LT_F,    LT_P,    KC_B,             KC_J,    LT_L,    KC_U,    KC_Y,    KC_SCLN,
+        MT_A,    MT_R,    MT_S,    MT_T,    KC_G,    MS_BTN2, KC_M,    MT_N,    MT_E,    MT_I,    MT_O,
+        KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,             KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,
+                          AE_MEH,  SCRL_MO, MT_TAB,  MS_BTN1, LT_SPC,  SCRL_TO, QK_REP
+    ),
+
+    [_FUN] = LAYOUT(
+        XXXXXXX, XXXXXXX, _______, UK_TOGG, QK_BOOT,          XXXXXXX, KC_F4,   KC_F5,   KC_F6,   XXXXXXX,
+        KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, XXXXXXX, _______, XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F12,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, KC_F7,   KC_F8,   KC_F9,   KC_F11,
+                          XXXXXXX, XXXXXXX, _______, _______, LT_F10,  XXXXXXX, XXXXXXX
+    ),
+
+    [_NUM] = LAYOUT(
+        XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX,          XXXXXXX, KC_4,    KC_5,    KC_6,    _______,
+        KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, XXXXXXX, _______, UM_GBP,  KC_1,    KC_2,    KC_3,    KC_BSLS,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          UM_EURO, KC_7,    KC_8,    KC_9,    KC_PIPE,
+                          XXXXXXX, XXXXXXX, _______, _______, LT_0,    XXXXXXX, XXXXXXX
+    ),
+
+    [_LSYM] = LAYOUT(
+        XXXXXXX, KC_PLUS, KC_UNDS, KC_DQUO, XXXXXXX,          XXXXXXX, _______, XXXXXXX, XXXXXXX, TG_MOUS,
+        KC_COLN, KC_EQL,  KC_MINS, KC_QUOT, XXXXXXX, _______, XXXXXXX, KC_LALT, KC_LGUI, KC_LSFT, KC_LCTL,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                          XXXXXXX, XXXXXXX, _______, _______, _______, XXXXXXX, XXXXXXX
+    ),
+
+    [_NAV] = LAYOUT(
+        XXXXXXX, PREVTAB, NEXTTAB, REDO,    CTL_BRK,          XXXXXXX, XXXXXXX, KC_DEL,  KC_BSPC, XXXXXXX,
+        KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, KC_ESC,  _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT,
+        UNDO,    CUT,     COPY,    PASTE,   XXXXXXX,          KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,
+                          XXXXXXX, XXXXXXX, MT_CWT,  _______, _______, XXXXXXX, XXXXXXX
+    ),
+
+    [_MOUSE] = LAYOUT(
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, RM_TOGG, XXXXXXX, XXXXXXX, TG_MOUS,
+        ROT_L15, ROT_R15, SCRL_IN, SCRL_SW, CPI_SW,  _______, RM_NEXT, RM_HUEU, RM_SATU, RM_VALU, RM_SPDU,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          RM_PREV, RM_HUED, RM_SATD, RM_VALD, RM_SPDD,
+                          XXXXXXX, _______, KC_LSFT, _______, XXXXXXX, _______, XXXXXXX
+    ),
+};
