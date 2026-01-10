8cf3669dec3297f383e6f4010c8b3712626b445e
abstracterror <drew@abstracterror.com>
2026-01-10 18:33:19 +0000

    Rework Vault 35 keymap for use with 3u bars

--- keyboards/projectcain/vault35/keymaps/abstracterror/keymap.c
+++ keyboards/projectcain/vault35/keymaps/abstracterror/keymap.c
@@ -30,41 +30,41 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,    KC_W,    LT_E,    LT_R,    KC_T,    KC_Y,    LT_U,    KC_I,    KC_O,    KC_P,    KC_F12,
         MT_A,    MT_S,    MT_D,    MT_F,    KC_G,    KC_H,    MT_J,    MT_K,    MT_L,    MT_SCLN,
         MT_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  MT_SLSH,
-                          KC_MEH,  LT_TAB,  MT_TAB,  LT_SPC,  MO_LSYM, KC_ENT
+                          AE_MEH,  LT_TAB,  MT_TAB,  LT_SPC,  MO_LSYM, QK_REP
     ),
 
     [_FUN] = LAYOUT_split_4space(
         XXXXXXX, XXXXXXX, XXXXXXX, UK_TOGG, QK_BOOT, XXXXXXX, KC_F4,   KC_F5,   KC_F6,   XXXXXXX, _______,
-        KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, XXXXXXX, XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F12,
+        KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, XXXXXXX, KC_F11 , KC_F1,   KC_F2,   KC_F3,   KC_F12,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F7,   KC_F8,   KC_F9,   XXXXXXX,
-                          XXXXXXX, XXXXXXX, _______, LT_F10,  KC_F11,  XXXXXXX
+                          _______, _______, _______, LT_F10,  _______, _______
     ),
 
     [_NUM] = LAYOUT_split_4space(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_4,    KC_5,    KC_6,    _______, XXXXXXX,
         KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, XXXXXXX, UM_GBP,  KC_1,    KC_2,    KC_3,    KC_BSLS,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, UM_EURO, KC_7,    KC_8,    KC_9,    KC_PIPE,
-                          XXXXXXX, XXXXXXX, _______, LT_0,    MO_TSYM, XXXXXXX
+                          _______, _______, _______, LT_0,    MO_TSYM, _______
     ),
 
     [_LSYM] = LAYOUT_split_4space(
         XXXXXXX, KC_PLUS, KC_UNDS, KC_DQUO, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         KC_COLN, KC_EQL,  KC_MINS, KC_QUOT, XXXXXXX, XXXXXXX, KC_LALT, KC_LGUI, KC_LSFT, KC_LCTL,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                          XXXXXXX, MO_TSYM, _______, _______, XXXXXXX, XXXXXXX
+                          _______, MO_TSYM, _______, _______, _______, _______
     ),
 
     [_TSYM] = LAYOUT_split_4space(
         KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_GRV,  KC_TILD, XXXXXXX,
         KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, XXXXXXX, RM_NEXT, RM_TOGG, RM_HUEU, RM_SATU, RM_VALU,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RM_PREV, XXXXXXX, RM_HUED, RM_SATD, RM_VALD,
-                          XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX
+                          _______, _______, _______, _______, _______, _______
     ),
 
     [_NAV] = LAYOUT_split_4space(
         XXXXXXX, PREVTAB, NEXTTAB, REDO,    CTL_BRK, XXXXXXX, XXXXXXX, KC_DEL,  KC_BSPC, XXXXXXX, XXXXXXX,
         KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, KC_ESC,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT,
         UNDO,    CUT,     COPY,    PASTE,   XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,
-                          XXXXXXX, XXXXXXX, MT_CWT,  _______, XXXXXXX, XXXXXXX
+                          _______, _______, MT_CWT,  _______, _______, _______
     ),
 };
