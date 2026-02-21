9708fbffb4e22908c62040487d71b83e0ddaaef6
abstracterror <drew@abstracterror.com>
2026-02-21 13:44:52 +0000

    Update Oracle keymap for revised LAYOUT macro
    LAYOUT now corresponds to the row-staggered layout with split bars. Also
    fixes the QK_REP binding by enabling the repeat key feature, and
    switches to my QAZ layout with '/' on R3.

--- keyboards/projectcain/oracle/keymaps/abstracterror/keymap.c
+++ keyboards/projectcain/oracle/keymaps/abstracterror/keymap.c
@@ -23,9 +23,9 @@
 const char PROGMEM chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] = LAYOUT(
     'L', 'L', 'L', 'L',    '*',    'R', 'R', 'R', 'R',
     'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
-    'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
-    'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
-         '*',      '*', '*',  '*', '*', '*',      '*'
+    'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R',
+    'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R',
+         '*',      '*', '*',  '*', '*',      '*'
 );
 
 #if defined(ENCODER_MAP_ENABLE)
@@ -43,40 +43,40 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
                  KC_ESC,  KC_F1,   KC_F2,   KC_F5,   RM_TOGG, KC_F8,   KC_F10,  KC_F11,  KC_BSPC,
         KC_Q,    KC_W,    LT_E,    LT_R,    KC_T,    KC_Y,    LT_U,    KC_I,    KC_O,    KC_P,    KC_F12,
-        MT_A,    MT_S,    MT_D,    MT_F,    KC_G,    KC_H,    MT_J,    MT_K,    MT_L,    XXXXXXX, MT_SCLN,
-        MT_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  XXXXXXX, MT_SLSH,
-                 XXXXXXX,          AE_MEH,  MT_TAB,  XXXXXXX, LT_SPC,  QK_REP,           XXXXXXX
+        MT_A,    MT_S,    MT_D,    MT_F,    KC_G,    KC_H,    MT_J,    MT_K,    MT_L,    MT_SLSH,
+        MT_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT,
+                 XXXXXXX,          AE_MEH,  MT_TAB,  LT_SPC,  QK_REP,           XXXXXXX
     ),
 
     [_FUN] = LAYOUT(
                  _______, _______, _______, _______, _______, _______, _______, _______, _______,
         XXXXXXX, XXXXXXX, _______, UK_TOGG, QK_BOOT, XXXXXXX, KC_F4,   KC_F5,   KC_F6,   XXXXXXX, _______,
-        KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, XXXXXXX, XXXXXXX, KC_F1,   KC_F2,   KC_F3,   XXXXXXX, KC_F12,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F7,   KC_F8,   KC_F9,   XXXXXXX, XXXXXXX,
-                 XXXXXXX,          XXXXXXX, _______, XXXXXXX, LT_F10,  KC_F11,           XXXXXXX
+        KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, XXXXXXX, XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F12,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F7,   KC_F8,   KC_F9,   XXXXXXX,
+                 XXXXXXX,          XXXXXXX, _______, LT_F10,  KC_F11,           XXXXXXX
     ),
 
     [_NUM] = LAYOUT(
                  _______, _______, _______, _______, _______, _______, _______, _______, _______,
         XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, KC_4,    KC_5,    KC_6,    _______, XXXXXXX,
-        KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, XXXXXXX, UM_GBP,  KC_1,    KC_2,    KC_3,    XXXXXXX, KC_BSLS,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, UM_EURO, KC_7,    KC_8,    KC_9,    XXXXXXX, KC_PIPE,
-                 XXXXXXX,          XXXXXXX, _______, XXXXXXX, LT_0,    _______,          XXXXXXX
+        KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, XXXXXXX, UM_GBP,  KC_1,    KC_2,    KC_3,    KC_BSLS,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, UM_EURO, KC_7,    KC_8,    KC_9,    KC_PIPE,
+                 XXXXXXX,          XXXXXXX, _______, LT_0,    _______,          XXXXXXX
     ),
 
     [_LSYM] = LAYOUT(
                  _______, _______, _______, _______, _______, _______, _______, _______, _______,
         XXXXXXX, KC_PLUS, KC_UNDS, KC_DQUO, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        KC_COLN, KC_EQL,  KC_MINS, KC_QUOT, XXXXXXX, XXXXXXX, KC_LALT, KC_LGUI, KC_LSFT, XXXXXXX, KC_LCTL,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                 XXXXXXX,          _______, _______, XXXXXXX, _______, XXXXXXX,          XXXXXXX
+        KC_COLN, KC_EQL,  KC_MINS, KC_QUOT, XXXXXXX, XXXXXXX, KC_LALT, KC_LGUI, KC_LSFT, KC_LCTL,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                 XXXXXXX,          _______, _______, _______, XXXXXXX,          XXXXXXX
     ),
 
     [_NAV] = LAYOUT(
                  _______, _______, _______, _______, _______, _______, _______, _______, _______,
         XXXXXXX, PREVTAB, NEXTTAB, REDO,    CTL_BRK, XXXXXXX, XXXXXXX, KC_DEL,  KC_BSPC, XXXXXXX, XXXXXXX,
-        KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, KC_ESC,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, KC_ENT,
-        UNDO,    CUT,     COPY,    PASTE,   XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, KC_INS,
-                 XXXXXXX,          XXXXXXX, KC_LSFT, XXXXXXX, _______, XXXXXXX,          XXXXXXX
+        KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, KC_ESC,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT,
+        UNDO,    CUT,     COPY,    PASTE,   XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,
+                 XXXXXXX,          XXXXXXX, KC_LSFT, _______, XXXXXXX,          XXXXXXX
     ),
 };
