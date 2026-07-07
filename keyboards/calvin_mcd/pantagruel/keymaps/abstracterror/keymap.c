12df02b09ca9c596e948c80147b582f7fef06003
abstracterror <drew@abstracterror.com>
2026-07-07 21:13:41 +0100

    Add keymap for calvin_mcd/pantagruel

---
+++ keyboards/calvin_mcd/pantagruel/keymaps/abstracterror/keymap.c
@@ -0,0 +1,53 @@
+// Copyright 2026 @abstracterror
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+#include "abstracterror.h"
+#include "ae_combos.c"
+#include "ae_keyoverrides.c"
+
+const char PROGMEM chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] = LAYOUT(
+    'L', 'L', 'L', 'L', 'L', '*', 'R', 'R', 'R', 'R', 'R',
+    'L', 'L', 'L', 'L', 'L',      'R', 'R', 'R', 'R', 'R',
+    'L', 'L', 'L', 'L', 'L',      'R', 'R', 'R', 'R', 'R',
+              '*', '*', '*', '*', '*', '*', '*'
+);
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    [_BASE] = LAYOUT(
+        KC_Q,    KC_W,    LT_F,    LT_P,    KC_B,    XXXXXXX, KC_J,    LT_L,    KC_U,    KC_Y,    KC_F12,
+        MT_A,    MT_R,    MT_S,    MT_T,    KC_G,             KC_M,    MT_N,    MT_E,    MT_I,    MT_O,
+        KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,             KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,
+                          XXXXXXX, AE_MEH,  MT_TAB,  XXXXXXX, LT_SPC,  QK_REP,  XXXXXXX
+    ),
+
+    [_NUM] = LAYOUT(
+        XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, _______, XXXXXXX, KC_4,    KC_5,    KC_6,    _______,
+        KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, XXXXXXX,          UM_GBP,  KC_1,    KC_2,    KC_3,    KC_BSLS,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          UM_EURO, KC_7,    KC_8,    KC_9,    KC_PIPE,
+                          _______, _______, KC_LSFT, _______, LT_0,     _______, _______
+    ),
+
+    [_FUN] = LAYOUT(
+        XXXXXXX, XXXXXXX, _______, UK_TOGG, QK_BOOT, _______, RM_SPDU, KC_F4,   KC_F5,   KC_F6,   XXXXXXX,
+        KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, XXXXXXX,          RM_SPDD, KC_F1,   KC_F2,   KC_F3,   KC_F12,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, KC_F7,   KC_F8,   KC_F9,   KC_F11,
+                          _______, _______, KC_LSFT, _______, LT_F10,  _______, _______
+
+    ),
+
+    [_LSYM] = LAYOUT(
+        XXXXXXX, KC_PLUS, KC_UNDS, KC_DQUO, XXXXXXX, _______, XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_COLN, KC_EQL,  KC_MINS, KC_QUOT, XXXXXXX,          XXXXXXX, KC_LALT, KC_LGUI, KC_LSFT, KC_LCTL,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                          _______, _______, KC_LSFT, _______, _______, _______, _______
+    ),
+
+    [_NAV] = LAYOUT(
+        XXXXXXX, PREVTAB, NEXTTAB, REDO,    CTL_BRK, _______, XXXXXXX, XXXXXXX, KC_DEL,  KC_BSPC, XXXXXXX,
+        KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, KC_ESC,           KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT,
+        UNDO,    CUT,     COPY,    PASTE,   XXXXXXX,          KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,
+                          _______, _______, KC_LSFT, _______, _______, _______, _______
+    ),
+};
